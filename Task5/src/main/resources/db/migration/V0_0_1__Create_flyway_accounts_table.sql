0d0c5472e56dda8cb4775a0bb405b25b1d7d45f6
roman-buzhovych <roman.buzhovych@equifax.com>
2022-02-08 12:00:02 +0200

    add task5

---
+++ Task5/src/main/resources/db/migration/V0_0_1__Create_flyway_accounts_table.sql
@@ -0,0 +1,1 @@
+create table flyway_accounts(id bigint, login varchar(255), password varchar(255), role varchar(30));
---
+++ Task5/src/main/resources/db/migration/V0_0_2__Add_test_data.sql
@@ -0,0 +1,2 @@
+insert into flyway_accounts values (1, 'admin', 'pass', 'ADMIN');
+insert into flyway_accounts values (2, 'user', 'pass', 'USER');
